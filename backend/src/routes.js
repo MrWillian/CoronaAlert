0f717b8e4a4db2873c3df9edb9c3812aa3fec984
MrWilllian <willian.dev10@gmail.com>
2020-03-17 17:19:59 -0300

    refactor: add post api user route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
 const routes = express.Router();
+
+const userController = require('../src/controllers/UserController');
+
+routes.post('/api/user', userController.store);
 
 module.exports = routes;
