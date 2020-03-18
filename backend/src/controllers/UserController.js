dd3616e923d8e86ed3a40ddc78ce3cbe91d703f2
MrWilllian <willian.dev10@gmail.com>
2020-03-17 21:51:19 -0300

    refactor: add login method

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,15 +2,25 @@ const User = require('../models/User');
 
 module.exports = {
   async store(req, res) {
-    console.log(req);
-    const { name, avatar, age } = req.body;
+    const { name, email, password, age, country, hasCovid } = req.body;
 
     const user = await User.create({
-      name, avatar, age, country
+      name, email, password, age, country, hasCovid
     });
 
     req.io.emit('newUser', res.json(user));
 
     return res.status(200).send();
+  },
+
+  async login(req, res) {
+    const { email, password } = req.body;
+
+    const userExists = await User.findOne({ email, password });
+    
+    if (userExists)
+      return res.json(userExists);
+    
+    return res.json('unauthorized');
   }
 }
