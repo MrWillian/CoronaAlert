6d66c85730d62cefd5d4e5e596276f2f3eae1fe7
MrWilllian <willian.dev10@gmail.com>
2020-03-17 21:53:07 -0300

    test: add Auth login test (incomplete)

--- backend/__tests__/integration/User/UserController.test.js
+++ backend/__tests__/integration/User/UserController.test.js
@@ -1,21 +1,35 @@
 const request = require('supertest');
 const app = require('../../../src/routes');
 
-describe('Register', () => {
-  it('should receive user object when store with success the user in database', async () => {
-    const user = {
-      name: 'Test User',
-      email: 'test@test.com',
-      age: 18,
-      country: 'Chile',
-      hasCovid: false, 
-    };
+// describe('Register', () => {
+//   it('should receive user object when store with success the user in database', async () => {
+//     const user = {
+//       name: 'Test User',
+//       email: 'test@test.com',
+//       age: 18,
+//       country: 'Chile',
+//       hasCovid: false, 
+//     };
     
-    const response = await request(app).post('/api/user').send({
+//     const response = await request(app).post('/api/user').send({
+//       user
+//     });
+
+//     expect(response.status).toBe(200);
+//   });
+// });
+
+describe('Auth', () => {
+  it('should receive authorized user object', async () => {
+    const user = {
+      email: 'admin@teste.com.br',
+      password: '123456',
+    };
+
+    const response = await request(app).post('/api/login').send({
       user
     });
 
     expect(response.status).toBe(200);
   });
 });
-
