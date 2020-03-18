19e2212d8f4407fe56248c41be07902de86adeb9
MrWilllian <willian.dev10@gmail.com>
2020-03-17 22:38:11 -0300

    refactor: add password hash insert in store and compare in login

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,11 +1,14 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   async store(req, res) {
     const { name, email, password, age, country, hasCovid } = req.body;
 
+    const hashPassword = bcrypt.hashSync(password, 10);
+
     const user = await User.create({
-      name, email, password, age, country, hasCovid
+      name, email, password: hashPassword, age, country, hasCovid
     });
 
     req.io.emit('newUser', res.json(user));
@@ -16,11 +19,16 @@ module.exports = {
   async login(req, res) {
     const { email, password } = req.body;
 
-    const userExists = await User.findOne({ email, password });
+    const userExists = await User.findOne({ email });
     
-    if (userExists)
-      return res.json(userExists);
-    
-    return res.json('unauthorized');
+    if (userExists) {
+      if(bcrypt.compareSync(password, userExists.password)) {
+        return res.json(userExists);
+      } else {
+        return res.json('unauthorized');
+      }
+    } else {
+      return res.json("user doesn't exist");
+    }
   }
 }
