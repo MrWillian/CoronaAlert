51a44b8a3b5c50aef56c6dd92009f8bff60550a1
MrWilllian <willian.dev10@gmail.com>
2020-03-19 08:36:36 -0300

    refactor: add try catch to login method

--- CoronaAlert/src/controllers/AuthController.js
+++ CoronaAlert/src/controllers/AuthController.js
@@ -1,9 +1,13 @@
 import api from '../services/api';
 
 const login = async (email, password) => {
-  const response = await api.post('/login', { email, password });
-  const {_id} = response.data;
-  return _id;
+  try {
+    const response = await api.post('/login', { email, password });
+    const {_id} = response.data;
+    return _id;
+  } catch(error) {
+    console.log('Usuário não existe!', error);
+  }
 }
 
 export { login }
