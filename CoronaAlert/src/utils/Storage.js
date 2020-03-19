a1f6030dfeed2db0b45da559ec2067f0872615e6
MrWilllian <willian.dev10@gmail.com>
2020-03-19 08:36:56 -0300

    refactor: add try catch to methods in Storage utils

--- CoronaAlert/src/utils/Storage.js
+++ CoronaAlert/src/utils/Storage.js
@@ -1,11 +1,19 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
 const setItemStorage = async (key, value) => {
-  await AsyncStorage.setItem(key, value);
+  try {
+    await AsyncStorage.setItem(key, value);    
+  } catch(error) {
+    console.log('Erro ao armazenar o usuário logado', error);
+  }
 }
 
 const getItemStorage = async key => {
-  return await AsyncStorage.getItem(key);
+  try {
+    return await AsyncStorage.getItem(key);
+  } catch(error) {
+    console.log('Erro ao recuperar o usuário logado', error);
+  }
 }
 
 export { setItemStorage, getItemStorage }
